52040a0f46de13040babdb6bef2b37fc5d427042
Nikita Sharapatov <qum1ch@mail.ru>
2026-05-28 13:23:15 +0300

    Initial Commit

---
+++ F1Statistics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  F1Statistics
+//
+//  Created by Никита Шарапатов on 28.05.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
